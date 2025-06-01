fab712d33b9913743fda65934a4559b9afadbf22
LucasAraujo1306 <lucasisaac007@gmail.com>
2025-06-01 12:40:55 -0300

    criacao do arquivo para a excecoes personalizada

---
+++ ConsumoExcedenteException.java
@@ -0,0 +1,10 @@
+package SistemaConsumoGas;
+
+public class ConsumoExcedenteException extends Exception {
+
+    // Construtor que recebe uma mensagem de erro
+    public ConsumoExcedenteException(String message) {
+        super(message);
+    }
+    
+}
